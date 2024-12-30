6fa4dc740bc518c7b4220ceb41898a8e099c6274
archig97 <archismitaghosh@yahoo.com>
2024-12-30 10:46:45 -0600

    adding local DB and dummy users and trains

--- app/src/main/java/ticket_booking/services/UserBookingService.java
+++ app/src/main/java/ticket_booking/services/UserBookingService.java
@@ -1,5 +1,23 @@
 package ticket_booking.services;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ticket_booking.entities.User;
+
 public class UserBookingService {
+	
+	private User user;
+	
+	private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+	
+	private static final String USERS_PATH = "../ticket_booking.localdb/users.json";
+	
+	public UserBookingService(User user) {
+		
+		this.user = user;
+		File users = new File(USERS_PATH);
+	}
 
 }
