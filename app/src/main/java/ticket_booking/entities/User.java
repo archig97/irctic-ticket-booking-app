efc7076ce7a3c9eb64ce84a2b7218c198a0c8364
archig97 <archismitaghosh@yahoo.com>
2024-12-29 21:57:48 -0600

    initial project setup

---
+++ app/src/main/java/ticket_booking/entities/User.java
@@ -0,0 +1,17 @@
+package ticket_booking.entities;
+
+import java.util.List;
+
+public class User {
+	
+	private String name;
+	
+	private String password;
+	
+	private String hashedPassword;
+	
+	private String userId;
+	
+	private List<Ticket> ticketsBooked;
+
+}
